c93116e023790a0c7cd0f047d71842a8895d4d07
Aurel-Charles <charlesaurelienpro@gmail.com>
2026-09-22 15:15:26 +0200

    add neon db

---
+++ db/queries.js
@@ -0,0 +1,29 @@
+import pool from "./pool.js";
+
+
+export async function getMessages() {
+    const { rows } = await pool.query("SELECT * FROM messages ORDER BY id");
+    return rows;
+  }
+
+export async function addMessageToDb(message) {
+    await pool.query(
+        "INSERT INTO messages (text, username) VALUES ($1, $2)",
+        [message.text, message.username]
+      );
+}
+
+export async function getMessageById(id) {
+    const { rows } = await pool.query(
+      "SELECT * FROM messages WHERE id = $1",
+      [id]
+    );
+    return rows[0] || null;
+  }
+
+export async function deleteMessage(id) {
+    await pool.query(
+        "DELETE FROM messages WHERE id = $1",
+        [id]
+    )
+}
